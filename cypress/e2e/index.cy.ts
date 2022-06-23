c823cbcae3225f566e7f9206c37c35c3857e2c5f
huecester <jericho.paulino@yahoo.com.ph>
2022-06-23 15:01:06 -0700

    Add test for active class in navbar

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -3,16 +3,25 @@ describe('/index', () => {
 		cy.visit('/');
 		cy.dataCy('navbar').as('navbar');
 
-		const LINKS = [ 
+		const ROUTES = [ 
 			{ name: 'Home', href: '/' },
 			{ name: 'About', href: '/about' },
 			{ name: 'Games', href: '/games' },
 			{ name: 'Bots', href: '/bots' },
 		];
 
-		for (const link of LINKS) {
-			cy.get('@navbar').contains(link.name).click();
-			cy.url().should('match', new RegExp(`${link.href}$`));
+		for (const route of ROUTES) {
+			cy.get('@navbar').contains(route.name).as('anchor')
+			cy.get('@anchor').click();
+
+			cy.get('@anchor').should('have.class', 'active');
+			cy.get('@navbar')
+				.get('ul')
+				.children()
+				.not(`:contains("${route.name}")`)
+				.each($el => cy.wrap($el).should('not.have.class', 'active'));
+
+			cy.url().should('match', new RegExp(`${route.href}$`));
 		}
 	});
 });
