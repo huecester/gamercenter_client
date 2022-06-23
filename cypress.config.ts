cf67cc67097b5076576ad0e1ca8f0e28959a7e4b
huecester <jericho.paulino@yahoo.com.ph>
2022-06-23 11:15:08 -0700

    Link with Cypress Dashboard

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,8 @@
+import 'dotenv/config';
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: process.env.CYPRESS_PROJECT_KEY,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
