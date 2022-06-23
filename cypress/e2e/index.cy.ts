621530ee33db59ffb204a7e8a8cdd5d597b7e976
huecester <jericho.paulino@yahoo.com.ph>
2022-06-23 12:26:46 -0700

    Add route testing

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -3,9 +3,16 @@ describe('/index', () => {
 		cy.visit('/');
 		cy.dataCy('navbar').as('navbar');
 
-		cy.get('@navbar').contains('Home').click();
-		cy.get('@navbar').contains('Games').click();
-		cy.get('@navbar').contains('About').click();
-		cy.get('@navbar').contains('Bots').click();
+		const LINKS = [ 
+			{ name: 'Home', href: '/' },
+			{ name: 'About', href: '/about' },
+			{ name: 'Games', href: '/games' },
+			{ name: 'Bots', href: '/bots' },
+		];
+
+		for (const link of LINKS) {
+			cy.get('@navbar').contains(link.name).click();
+			cy.url().should('match', new RegExp(`${link.href}$`));
+		}
 	});
 });
