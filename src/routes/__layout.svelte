f7792ea8168046c6817940b96012084acca15007
huecester <jericho.paulino@yahoo.com.ph>
2022-06-23 12:27:39 -0700

    Naming conventions

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import Header from '$lib/components/header.svelte';
-	import Navbar from '$lib/components/navbar.svelte';
+	import Header from '$lib/components/Header.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
 <Header />
