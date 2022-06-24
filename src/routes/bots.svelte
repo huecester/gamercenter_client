999e2abbeb15eaa2c3bf1db42095b99f81182645
huecester <jericho.paulino@yahoo.com.ph>
2022-06-24 12:15:47 -0700

    Move article rules to global style

--- src/routes/bots.svelte
+++ src/routes/bots.svelte
@@ -28,22 +28,3 @@
 {:else}
 	<IconLoading />
 {/if}
-
-<style lang="scss">
-	h2 {
-		margin: 0;
-	}
-	article {
-		background-color: variables.$nord1;
-		padding: 1rem;
-		border: 2px solid variables.$nord2;
-
-		& > a {
-			color: inherit;
-			text-decoration: inherit;
-			display: inline-block;
-			background-color: variables.$nord10;
-			padding: .75rem;
-		}
-	}
-</style>
